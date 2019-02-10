abf0db9d1a79ab52400132b2db3d477b35ed29ea
Ezeikel Pemberton <epemberton@discoveryed.com>
2019-02-10 17:55:24 +0000

    Updated api endpoint for dev/prod.

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,4 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
-export const endpoint = `http://localhost:4444`;
-export const prodEndpoint = `https://sickfits-yoga-prod.herokuapp.com/`;
+export const endpoint = `http://localhost:4466`;
+export const prodEndpoint = `http://http://209.97.134.152/`;
 export const perPage = 4;
