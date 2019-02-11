1e6ca9fa8718f59057eb255cb21b00beca8be659
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2019-02-11 00:01:51 +0000

    Updated domain for api.

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,4 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:4000`;
-export const prodEndpoint = `http://http://209.97.134.152/`;
+export const prodEndpoint = `http://api.crownd.co.uk`;
 export const perPage = 4;
