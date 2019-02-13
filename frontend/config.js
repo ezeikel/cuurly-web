d885a5a4bcef5e5f962e9dc69ddc153be1347c80
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2019-02-13 00:54:55 +0000

    Updated production api endpoint.

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,4 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:4000`;
-export const prodEndpoint = `http://api.crownd.co.uk`;
+export const prodEndpoint = `https://api.crownd.app`;
 export const perPage = 4;
