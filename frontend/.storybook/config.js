61483b28e8df76c690d6adcd31a37e0c8ffbdb0c
Ezeikel Pemberton <epemberton@discoveryed.com>
2019-02-25 12:02:53 +0000

    Added Decorator for GlobalStyle to stories.

--- frontend/.storybook/config.js
+++ frontend/.storybook/config.js
@@ -1,8 +1,18 @@
-import { configure } from '@storybook/react';
+import { Fragment } from 'react';
+import { configure, addDecorator } from '@storybook/react';
+import GlobalStyle from '../GlobalStyle';
 
 function loadStories() {
   require('../stories/index.js');
   // You can require as many stories as you need.
 }
 
+const withGlobal = cb => (
+  <Fragment>
+    <GlobalStyle />
+    {cb()}
+  </Fragment>
+);
+
+addDecorator(withGlobal);
 configure(loadStories, module);
