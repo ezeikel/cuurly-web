265f51f441f6fabfe67d982c20836a331758394c
Ezeikel Pemberton <epemberton@discoveryed.com>
2019-03-13 16:57:14 +0000

    users query takes where to enable user search from Search component on frontend.

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -2,7 +2,6 @@ const { isLoggedIn, hasPermission } = require('../utils');
 
 const Query = {
   currentUser: (_, args, ctx, info) => {
-    // check if there is current userId
     if (!ctx.request.userId) {
       return null;
     }
@@ -10,15 +9,14 @@ const Query = {
       id: ctx.request.userId
     }, info);
   },
-  users: (_, args, ctx, info) => {
-    // 1. check if they are logged in
+  users: (_, { where }, ctx, info) => {
     isLoggedIn(ctx);
 
-    // 2. check if the user has the permissions to query all the users
     hasPermission(ctx.request.user, ['ADMIN', 'PERMISSIONUPATE']);
 
-    // 3. if they do, query all the users
-    return ctx.prisma.users({}, info);
+    // just filtering on username for now until OR is added back for mongo connector - https://github.com/prisma/prisma/issues/3897
+
+    return ctx.prisma.users({ where }, info);
   },
   user: (_, { id }, ctx, info) => ctx.prisma.user(({ id }), info),
   userz: (_, args, ctx, info) => ctx.prisma.users({}, info),
--- frontend/components/Search.js
+++ frontend/components/Search.js
@@ -1,4 +1,9 @@
+import { Component } from 'react';
+import Router from 'next/router';
+import { ApolloConsumer } from 'react-apollo';
 import styled from 'styled-components';
+import debounce from 'lodash.debounce';
+import Downshift, { resetIdCounter } from 'downshift';
 
 const Wrapper = styled.div`
   display: grid;
