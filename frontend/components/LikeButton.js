04c04bdf13d06524c1b3269a5133c6468a450ef9
Ezeikel Pemberton <epemberton@discoveryed.com>
2019-03-26 15:44:02 +0000

    OptimisticReponse for LikeButton.

--- frontend/components/LikeButton.js
+++ frontend/components/LikeButton.js
@@ -17,17 +17,27 @@ const StyledButton = styled(Button)`
 
 const LikeButton = ({ postId, postLikes }) => (
   <CurrentUser>
-    {({ data: { currentUser } }) => (
-      currentUser ?
+    {({ data: { currentUser } }) => {
+      const mutate = postLikes.map(like => like.user.id).includes(currentUser.id) ? 'unlikePost' : 'likePost';
+      const id = postLikes.map(like => like.user.id).includes(currentUser.id) ? postLikes.filter(like  => like.user.id === currentUser.id)[0].id : postId;
+
+      return currentUser ?
         <Mutation SINGLE_USER_QUERY
           mutation={postLikes.map(like => like.user.id).includes(currentUser.id) ? UNLIKE_POST_MUTATION : LIKE_POST_MUTATION}
           variables={postLikes.map(like => like.user.id).includes(currentUser.id) ? { id: postLikes.filter(like  => like.user.id === currentUser.id)[0].id } : { id: postId }}
+          optimisticResponse={{ // https://www.apollographql.com/docs/react/features/optimistic-ui
+            __typeName: 'Mutation',
+              [mutate]: {
+                __typename: 'Like',
+                id
+              }
+          }}
           refetchQueries={[{ query: SINGLE_POST_QUERY, variables: { id: postId } }, { query: LIKED_POSTS_QUERY, variables: { id: currentUser.id } }, { query: SINGLE_USER_QUERY, variables: { id: currentUser.id } } ]}
         >
-          { like => <StyledButton mode={postLikes.map(like => like.user.id).includes(currentUser.id) ? 'unlike' : 'like'} onClick={like}>{ postLikes.map(like => like.user.id).includes(currentUser.id) ? 'Unlike' : 'Like' }</StyledButton> }
+          { (mutate, {loading, error }) => <StyledButton mode={postLikes.map(like => like.user.id).includes(currentUser.id) ? 'unlike' : 'like'} onClick={mutate} disabled={loading}>{ postLikes.map(like => like.user.id).includes(currentUser.id) ? 'Unlike' : 'Like' }</StyledButton> }
         </Mutation>
       : null
-    )}
+    }}
   </CurrentUser>
 );
 
