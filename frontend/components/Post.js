19a9584a77af30b6577d252498f4ab87fe7205c5
Ezeikel Pemberton <epemberton@discoveryed.com>
2019-04-12 13:23:54 +0100

    Removed post actions from comment to post header.

--- frontend/components/Post.js
+++ frontend/components/Post.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Link from "next/link";
 import { Component } from 'react';
 import { Query } from 'react-apollo';
@@ -9,6 +10,8 @@ import CommentList from './styles/CommentList';
 import Comment from './Comment';
 import DeletePost from './DeletePost';
 import formatDistance from 'date-fns/formatDistance';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Modal from 'react-modal';
 
 const Wrapper = styled.article`
   display: grid;
@@ -21,9 +24,10 @@ const Wrapper = styled.article`
 
 const PostHeader = styled.header`
   display: grid;
-  grid-template-columns: 30px 1fr;
+  grid-template-columns: 30px 1fr auto;
   grid-template-rows: 30px;
   grid-column-gap: var(--spacing-small);
+  align-items: center;
   padding: var(--spacing-medium);
   border-bottom: 1px solid #efefef;
   border-bottom-width: 0.5px;
@@ -96,60 +100,142 @@ const AddComment = styled.section`
   min-height: 56px;
 `;
 
-class Post extends Component {
-  render() {
-    return (
-      <Query query={SINGLE_POST_QUERY} variables={{ id: this.props.id }}>
-        {({ data: { post }, error, loading }) => {
-          if (loading) return <p>Loading...</p>;
-          if (error) return <p>Error: {error.message}</p>;
+const ReactModalAdapter = ({ className, modalClassName, ...props }) => (
+  <Modal
+    className={modalClassName}
+    portalClassName={className}
+    {...props}
+  />
+);
 
-          return (
-            <Wrapper>
-              <PostHeader>
-                <Photo>
-                  <img src={post.author.profilePicture} />
-                </Photo>
-                <Details>
-                  <Username>{post.author.username}</Username>
-                  <Location>Random post location</Location>
-                </Details>
-              </PostHeader>
-              <PostContent>
-                <img src={post.content.url.replace('/upload', '/upload/w_614,ar_4:5,c_limit,dpr_2.0')} />
-              </PostContent>
-              <PostInteraction>
-                <Buttons>
-                  <LikeButton postId={this.props.id} postLikes={post.likes} />
-                </Buttons>
-                {post.likes.length ? <Likes>{post.likes.length} like{post.likes.length > 1 ? 's' : null}</Likes> : null }
-                <Caption>
-                  <Link href={`/user?id=${post.author.id}`}>
-                    <a>{post.author.username}</a>
-                  </Link>
-                  {post.caption}
-                </Caption>
-                <CommentList>
-                  {post.comments.map(comment => (
-                    <Comment key={comment.id} comment={comment} post={post} />
-                  ))}
-                </CommentList>
-                <PostTime>
-                  {formatDistance(post.createdAt, new Date(), {
-                    includeSeconds: true
-                  })} ago
-                </PostTime>
-                <AddComment>
-                  <PostComment postId={post.id} />
-                </AddComment>
-                {/* <DeletePost post={post} /> */}
-              </PostInteraction>
-            </Wrapper>
-          );
-        }}
-      </Query>
-    );
+const StyledModal = styled(ReactModalAdapter).attrs({ //https://github.com/styled-components/styled-components/issues/1494
+  overlayClassName: 'overlay',
+  modalClassName: 'modal'
+})`
+  /* Portal styles here (though usually you will have none) */
+
+  .overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background: rgba(0,0,0, 0.5);
+    display: grid;
+    place-items: center;
   }
+  .modal {
+    display: grid;
+    background-color: var(--color-white);
+    min-height: 200px;
+    max-height: 400px;
+    width: 400px;
+    border-radius: 4px;
+    outline: 0;
+  }
+`;
+
+if (typeof (window) !== 'undefined') {
+  Modal.setAppElement('body');
+}
+
+const ModalBody = styled.div`
+  overflow-y: scroll;
+`;
+
+const PostActions = styled.ul `
+  display: grid;
+`;
+
+const PostAction = styled.li `
+  display: grid;
+  place-items: center;
+  min-height: 48px;
+  padding: 4px 8px;
+  line-height: 1.5;
+  & + li {
+    border-top: 1px solid #efefef;
+  }
+`;
+
+const StyledFontAwesomeIcon = styled(FontAwesomeIcon)`
+  cursor: pointer;
+`;
+
+const Post = ({ id }) => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const openModal = () => setModalIsOpen(true);
+  const closeModal = () => setModalIsOpen(false);
+
+  return (
+    <Query query={SINGLE_POST_QUERY} variables={{ id }}>
+      {({ data: { post }, error, loading }) => {
+        if (loading) return <p>Loading...</p>;
+        if (error) return <p>Error: {error.message}</p>;
+
+        return (
+          <Wrapper>
+            <PostHeader>
+              <Photo>
+                <img src={post.author.profilePicture} />
+              </Photo>
+              <Details>
+                <Username>{post.author.username}</Username>
+                <Location>Random post location</Location>
+              </Details>
+              <StyledFontAwesomeIcon onClick={openModal} icon={["far", "ellipsis-h"]} color="var(--color-black)" size="lg" />
+              <StyledModal
+                isOpen={modalIsOpen}
+                onRequestClose={closeModal}
+                conentLabel="Post Actions"
+              >
+              <ModalBody>
+                <PostActions>
+                  <PostAction>Unfollow</PostAction>
+                  <PostAction>Go to Post</PostAction>
+                  <PostAction>Copy Post</PostAction>
+                  <PostAction>Share</PostAction>
+                  <PostAction>Delete</PostAction>
+                  <PostAction>Cancel</PostAction>
+                </PostActions>
+              </ModalBody>
+              </StyledModal>
+            </PostHeader>
+            <PostContent>
+              <img src={post.content.url.replace('/upload', '/upload/w_614,ar_4:5,c_limit,dpr_2.0')} />
+            </PostContent>
+            <PostInteraction>
+              <Buttons>
+                <LikeButton postId={id} postLikes={post.likes} />
+              </Buttons>
+              {post.likes.length ? <Likes>{post.likes.length} like{post.likes.length > 1 ? 's' : null}</Likes> : null }
+              <Caption>
+                <Link href={`/user?id=${post.author.id}`}>
+                  <a>{post.author.username}</a>
+                </Link>
+                {post.caption}
+              </Caption>
+              <CommentList>
+                {post.comments.map(comment => (
+                  <Comment key={comment.id} comment={comment} post={post} />
+                ))}
+              </CommentList>
+              <PostTime>
+                {formatDistance(post.createdAt, new Date(), {
+                  includeSeconds: true
+                })} ago
+              </PostTime>
+              <AddComment>
+                <PostComment postId={post.id} />
+              </AddComment>
+              {/* <DeletePost post={post} /> */}
+            </PostInteraction>
+          </Wrapper>
+        );
+      }}
+    </Query>
+  );
 }
 
 export default Post;
--- frontend/components/PostComment.js
+++ frontend/components/PostComment.js
@@ -1,15 +1,10 @@
-import { useState } from 'react';
 import { Mutation } from 'react-apollo';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup'
 import CurrentUser from "./CurrentUser";
 import { SINGLE_POST_QUERY, ADD_COMMENT_MUTATION } from '../apollo/queries';
-import Button from './styles/Button';
-import Spinner from './Spinner';
 import { Fragment } from 'react';
 import styled from "styled-components";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import Modal from 'react-modal';
 
 const CommentSchema = Yup.object().shape({
   text: Yup.string()
@@ -34,70 +29,7 @@ const StyledForm = styled(Form)`
   cursor: pointer;
 `;
 
-const ReactModalAdapter = ({ className, modalClassName, ...props }) => (
-  <Modal
-    className={modalClassName}
-    portalClassName={className}
-    {...props}
-  />
-);
-
-const StyledModal = styled(ReactModalAdapter).attrs({ //https://github.com/styled-components/styled-components/issues/1494
-  overlayClassName: 'overlay',
-  modalClassName: 'modal'
-})`
-  /* Portal styles here (though usually you will have none) */
-
-  .overlay {
-    position: fixed;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background: rgba(0,0,0, 0.5);
-    display: grid;
-    place-items: center;
-  }
-  .modal {
-    display: grid;
-    background-color: var(--color-white);
-    min-height: 200px;
-    max-height: 400px;
-    width: 400px;
-    border-radius: 4px;
-    outline: 0;
-  }
-`;
-
-if (typeof (window) !== 'undefined') {
-  Modal.setAppElement('body');
-}
-
-const ModalBody = styled.div`
-  overflow-y: scroll;
-`;
-
-const PostActions = styled.ul`
-  display: grid;
-`;
-
-const PostAction = styled.li`
-  display: grid;
-  place-items: center;
-  min-height: 48px;
-  padding: 4px 8px;
-  line-height: 1.5;
-  & + li {
-    border-top: 1px solid #efefef;
-  }
-`;
-
 const PostComment = ({ postId }) => {
-  const [modalIsOpen, setModalIsOpen] = useState(false);
-
-  const openModal = () => setModalIsOpen(true);
-  const closeModal = () => setModalIsOpen(false);
-
   return (
     <CurrentUser>
       {({ data: { currentUser } }) => (
@@ -125,23 +57,6 @@ const PostComment = ({ postId }) => {
                   }) => (
                     <StyledForm>
                       <StyledField type="text" name="text" placeholder="Add a comment..." />
-                      <FontAwesomeIcon onClick={openModal} icon={["far", "ellipsis-h"]} color="var(--color-black)" size="lg" />
-                      <StyledModal
-                        isOpen={modalIsOpen}
-                        onRequestClose={closeModal}
-                        conentLabel="Post Actions"
-                      >
-                      <ModalBody>
-                        <PostActions>
-                          <PostAction>Unfollow</PostAction>
-                          <PostAction>Go to Post</PostAction>
-                          <PostAction>Delete Post</PostAction>
-                          <PostAction>Share</PostAction>
-                          <PostAction>Copy Post</PostAction>
-                          <PostAction>Cancel</PostAction>
-                        </PostActions>
-                      </ModalBody>
-                      </StyledModal>
                       <ErrorMessage name="text" />
                     </StyledForm>
                   )}
