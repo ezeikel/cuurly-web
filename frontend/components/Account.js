40e3ddb78d7a34ab4047d654cdbb70b7da1df785
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2019-04-18 09:03:02 +0100

    Fixed isEqual function.

--- frontend/components/Account.js
+++ frontend/components/Account.js
@@ -8,45 +8,33 @@ import Spinner from "./Spinner";
 import Button from "./styles/Button";
 import { CURRENT_USER_QUERY, SINGLE_USER_QUERY, UPDATE_USER_MUTATION } from "../apollo/queries";
 
-var isEqual = function (value, other) {
+function isEqual(a, b) {
+  console.log(a);
+  console.log(b);
+  // Create arrays of property names
+  var aProps = Object.getOwnPropertyNames(a);
+  var bProps = Object.getOwnPropertyNames(b);
 
-  // Get the value type
-  var type = Object.prototype.toString.call(value);
+  // If number of properties is different,
+  // objects are not equivalent
+  if (aProps.length != bProps.length) {
+    return false;
+  }
 
-  // If the two objects are not the same type, return false
-  if (type !== Object.prototype.toString.call(other)) return false;
+  for (var i = 0; i < aProps.length; i++) {
+    var propName = aProps[i];
 
-  // If items are not an object or array, return false
-  if (['[object Array]', '[object Object]'].indexOf(type) < 0) return false;
-
-  // Compare the length of the length of the two items
-  var valueLen = type === '[object Array]' ? value.length : Object.keys(value).length;
-  var otherLen = type === '[object Array]' ? other.length : Object.keys(other).length;
-  if (valueLen !== otherLen) return false;
-
-  // Compare two items
-  var compare = function (item1, item2) {
-    // Code will go here...
-  };
-
-  // Compare properties
-  var match;
-  if (type === '[object Array]') {
-    for (var i = 0; i < valueLen; i++) {
-      compare(value[i], other[i]);
-    }
-  } else {
-    for (var key in value) {
-      if (value.hasOwnProperty(key)) {
-        compare(value[key], other[key]);
-      }
+    // If values of same property are not equal,
+    // objects are not equivalent
+    if (a[propName] !== b[propName]) {
+      return false;
     }
   }
 
-  // If nothing failed, return true
+  // If we made it this far, objects
+  // are considered equivalent
   return true;
-
-};
+}
 
 const EditProfileSchema = Yup.object().shape({
   name: Yup.string()
