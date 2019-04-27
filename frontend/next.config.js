10baf803cf7003a8f0060fb398633cd5163e702b
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2019-04-27 15:32:17 +0100

    Fixed issue with now_launcher caused by withCSS.

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,23 @@
 // next.config.js
-const withCSS = require('@zeit/next-css');
 
-module.exports = withCSS({
-  target: "serverless",
-});
+// https://github.com/zeit/next.js/issues/5750
+const { PHASE_PRODUCTION_SERVER } =
+  process.env.NODE_ENV === 'development'
+    ? {} // We're never in "production server" phase when in development mode
+    : !process.env.NOW_REGION
+      ? require('next/constants') // Get values from `next` package when building locally
+      : require('next-server/constants'); // Get values from `next-server` package when building on now v2
+
+module.exports = (phase, { defaultConfig }) => {
+  if (phase === PHASE_PRODUCTION_SERVER) {
+    // Config used to run in production.
+    return {};
+  }
+
+  const withCSS = require('@zeit/next-css');
+
+  return withCSS({
+    target: "serverless",
+  });
+
+};
