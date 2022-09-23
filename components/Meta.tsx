5a68ff629a44efa59d67f81c9bb6e5f30cdeab3e
Ezeikel Pemberton <hi@ezeikel.dev>
2022-09-23 12:34:48 +0100

    chore: updated modules.

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -5,7 +5,6 @@ const Meta = () => (
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta charSet="utf-8" />
     <link rel="stylesheet" href="https://use.typekit.net/pim5iel.css" />
-    <link rel="stylesheet" type="text/css" href="/nprogress.css" />
     <title>
       Cuurly - A community for all things black and mixed textured hair.
     </title>
