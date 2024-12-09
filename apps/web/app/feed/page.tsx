74a2dc98eca21ec2c3872419b4ab3dda968853e0
Ezeikel Pemberton <hi@ezeikel.dev>
2024-12-09 01:15:41 +0000

    chore: frozen lockfile for workflow.

--- apps/web/app/feed/page.tsx
+++ apps/web/app/feed/page.tsx
@@ -1,12 +1,10 @@
 import Feed from "@/components/Feed/Feed";
 
-const FeedPage = () => {
-  return (
-    <div className="flex flex-col gap-4">
-      <h1 className="text-4xl font-bold">Feed Page</h1>
-      <Feed />
-    </div>
-  );
-};
+const FeedPage = () => (
+  <div className="flex flex-col gap-4">
+    <h1 className="text-4xl font-bold">Feed Page</h1>
+    <Feed />
+  </div>
+);
 
 export default FeedPage;
