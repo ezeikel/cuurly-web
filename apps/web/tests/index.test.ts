dbbf24c4727b4994664215b034acb1885ea493b9
Ezeikel Pemberton <hi@ezeikel.dev>
2024-12-11 02:29:03 +0000

    chore: updated screenshots path.

--- apps/web/tests/index.test.ts
+++ apps/web/tests/index.test.ts
@@ -3,7 +3,10 @@ import { expect, test } from "@playwright/test";
 test("basic test", async ({ page }) => {
   await page.goto("http://localhost:3000");
 
-  await page.screenshot({ path: "screenshots/screenshot.png", fullPage: true });
+  await page.screenshot({
+    path: "tests/screenshots/screenshot.png",
+    fullPage: true,
+  });
 
   const title = await page.title();
   expect(title).toBe(
