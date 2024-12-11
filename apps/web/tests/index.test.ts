39fb0007df6f00fdbb0bb6102ae4dfaca86858bb
Ezeikel Pemberton <hi@ezeikel.dev>
2024-12-11 01:52:49 +0000

    chore: simple e2e test.

---
+++ apps/web/tests/index.test.ts
@@ -0,0 +1,12 @@
+import { expect, test } from "@playwright/test";
+
+test("basic test", async ({ page }) => {
+  await page.goto("http://localhost:3000");
+
+  await page.screenshot({ path: "screenshots/screenshot.png", fullPage: true });
+
+  const title = await page.title();
+  expect(title).toBe(
+    "Cuurly - A community for all things black and mixed textured hair.",
+  );
+});
